11bef6d0cf57e2ff77cabb145500bb7ec2ad0788
UzDevid <uzdevid@gmail.com>
2023-04-20 18:39:13 +0500

    Добавил проверку

--- FcmAsset.php
+++ FcmAsset.php
@@ -3,6 +3,7 @@
 namespace uzdevid\fcm;
 
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\web\AssetBundle;
 use yii\web\View;
 
@@ -17,6 +18,10 @@ class FcmAsset extends AssetBundle {
     ];
 
     public function registerAssetFiles($view) {
+        if (empty(Yii::$app->params['fcm'])) {
+            throw new InvalidConfigException(Yii::t('app', 'Firebase config not found'));
+        }
+
         $script = "const firebaseConfig = " . json_encode(Yii::$app->params['fcm']) . ";";
         $view->registerJs($script, View::POS_HEAD);
 
